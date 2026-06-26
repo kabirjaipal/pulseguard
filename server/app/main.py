a180d04b564249777ae6f2715691e117b2252c2b
Kabir Jaipal <73753258+kabirjaipal@users.noreply.github.com>
2026-06-26 22:51:18 +0530

    deployment

--- server/app/main.py
+++ server/app/main.py
@@ -1,5 +1,9 @@
 import os
 import sys
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Ensure the root of the server directory is in sys.path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
